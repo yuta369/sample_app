134706b055d42b1dee871cf1bb1d0c7ce4fe6ea2
yuta369 <y.m.36912@gmail.com>
2024-05-10 10:50:22 +0000

    [Finish] 3-6 [Add] 画像投稿機能の追加

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -24,16 +24,16 @@ class ListsController < ApplicationController
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
-    redirect_to list_path(list.id)  
+    redirect_to list_path(list.id)
   end
 
   private
   # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
